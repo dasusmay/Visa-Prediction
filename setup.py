ed951a5db57ba8efce942e4ff5962e79d154c936
Susmay <susmoydas1000@gmail.com>
2025-08-24 13:28:45 -0600

    requirement setup done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Susmay",
+    author_email="susmoydas1000@gmail.com",
+    packages=find_packages()
+)
